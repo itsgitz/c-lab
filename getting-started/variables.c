7983e50fbca1052fdaaaf1250cbbe0d423507f63
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-11-09 09:12:05 +0700

    chore: added float and char data types

--- getting-started/variables.c
+++ getting-started/variables.c
@@ -4,9 +4,13 @@ int
 main ()
 {
   int myNumber = 15;
+  float myFloatNumber = 5.99;
+  char myLetter = 'D';
 
   printf ("Let's learn variables! \n");
-  printf ("My number: %i", myNumber);
+  printf ("My number: %d \n", myNumber);
+  printf ("My float number: %f\n", myFloatNumber);
+  printf ("My letter: %c\n", myLetter);
 
   return 0;
 }
