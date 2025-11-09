7a2d71f84011ce45d125d9be5e085f39ab9de6e6
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-11-09 11:06:54 +0700

    chore: sum the numbers

--- getting-started/variables.c
+++ getting-started/variables.c
@@ -6,11 +6,15 @@ main ()
   int myNumber = 15;
   float myFloatNumber = 5.99;
   char myLetter = 'D';
+  int x = 5;
+  int y = 10;
+  int sum = x + y;
 
   printf ("Let's learn variables! \n");
   printf ("My number: %d \n", myNumber);
   printf ("My float number: %f\n", myFloatNumber);
   printf ("My letter: %c\n", myLetter);
+  printf ("x + y = %d \n", sum);
 
   return 0;
 }
